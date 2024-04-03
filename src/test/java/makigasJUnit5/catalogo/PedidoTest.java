7597caf476ead1385728300d8a3cdc93e3a5f630
alcarazaina <alcarazdainara@gmail.com>
2024-04-03 17:29:22 +0200

    update

--- src/test/java/makigasJUnit5/catalogo/PedidoTest.java
+++ src/test/java/makigasJUnit5/catalogo/PedidoTest.java
@@ -1,9 +1,6 @@
 package makigasJUnit5.catalogo;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,20 +19,35 @@ class PedidoTest {
     }
 
     @Test
+    @DisplayName("Pedido con 0 productos")
     void cantidad() {
+        Pedido p = new Pedido();
+        //Vamos a probar que cuando el pedido está vacío la cantidad que devuelve es 0.
+        Assertions.assertEquals(0, p.cantidad());
+
+    }
+    @Test
+    @DisplayName("Pedido con 1 producto")
+    void cantidad2() {
+        // Para probar el método cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
+        Producto p1 = new Producto("Coco", 400);
+
+        Pedido p = new Pedido();
+        p.agregar(p1);
+        var total = p.cantidad();
+        Assertions.assertEquals(1, total);
+    }
+    @Test
+    @DisplayName("Pedido con 2 productos")
+    void cantidad3() {
         // Para probar el método cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
         Producto p1 = new Producto("Coco", 400);
         Producto p2 = new Producto("Canela", 200);
 
         Pedido p = new Pedido();
-        //Vamos a probar que cuando el pedido está vacío la cantidad que devuelve es 0.
-        Assertions.assertEquals(0, p.cantidad());
         p.agregar(p1);
-        var total = p.cantidad();
-        Assertions.assertEquals(1, total);
         p.agregar(p2);
-        total = p.cantidad();
-        Assertions.assertEquals(2, total);
+        Assertions.assertEquals(2, p.cantidad());
 
 
     }
