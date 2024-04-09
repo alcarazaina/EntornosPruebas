20ffef697e47e48c457965b68b1758c7dfd10002
alcarazaina <alcarazdainara@gmail.com>
2024-04-09 18:13:48 +0200

    update

--- src/test/java/makigasJUnit5/catalogo/PedidoTest.java
+++ src/test/java/makigasJUnit5/catalogo/PedidoTest.java
@@ -6,12 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PedidoTest {
 
+    Producto p1, p2;
+    Pedido p;
     @BeforeEach
     void setUp() {
+        Producto p1 = new Producto("Coco", 400);
+        Producto p2 = new Producto("Canela", 200);
+        p = new Pedido();
     }
 
     @AfterEach
     void tearDown() {
+        p.vaciar();
     }
 
     @Test
@@ -21,7 +27,6 @@ class PedidoTest {
     @Test
     @DisplayName("Pedido con 0 productos")
     void cantidad() {
-        Pedido p = new Pedido();
         //Vamos a probar que cuando el pedido está vacío la cantidad que devuelve es 0.
         Assertions.assertEquals(0, p.cantidad());
 
@@ -30,9 +35,6 @@ class PedidoTest {
     @DisplayName("Pedido con 1 producto")
     void cantidad2() {
         // Para probar el método cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
-        Producto p1 = new Producto("Coco", 400);
-
-        Pedido p = new Pedido();
         p.agregar(p1);
         var total = p.cantidad();
         Assertions.assertEquals(1, total);
@@ -41,10 +43,6 @@ class PedidoTest {
     @DisplayName("Pedido con 2 productos")
     void cantidad3() {
         // Para probar el método cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
-        Producto p1 = new Producto("Coco", 400);
-        Producto p2 = new Producto("Canela", 200);
-
-        Pedido p = new Pedido();
         p.agregar(p1);
         p.agregar(p2);
         Assertions.assertEquals(2, p.cantidad());
