617b3747dc238e24e8caeca0fc6cad273dfdecb2
alcarazaina <alcarazdainara@gmail.com>
2024-04-15 16:20:36 +0200

    update

---
+++ src/main/java/ejercicioscajanegra/ejercicio01/Excursion.java
@@ -0,0 +1,27 @@
+package ejercicioscajanegra.ejercicio01;
+
+public class Excursion {
+    private int numeroAlumnos;
+
+    public Excursion(int numeroAlumnos) {
+        this.numeroAlumnos = numeroAlumnos;
+    }
+
+    public int getNumeroAlumnos() {
+        return numeroAlumnos;
+    }
+
+    public void setNumeroAlumnos(int numeroAlumnos) {
+        this.numeroAlumnos = numeroAlumnos;
+    }
+
+    // métodos de instancia
+
+    public double calcularPrecioAlumno(){
+        return 0;
+    }
+
+    public double calcularCosteExcursion(){
+        return 0;
+    }
+}
---
+++ src/main/java/ejercicioscajanegra/ejercicio01/ExcursionMetodosEstaticos.java
@@ -0,0 +1,11 @@
+package ejercicioscajanegra.ejercicio01;
+
+public class ExcursionMetodosEstaticos {
+    public static double calcularPrecioAlumno(int numeroAlumnos){
+        return 0;
+    }
+
+    public static double calcularCosteExcursion(int numeroAlumnos){
+        return 0;
+    }
+}
