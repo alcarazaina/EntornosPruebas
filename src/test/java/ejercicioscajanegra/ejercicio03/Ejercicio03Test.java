e8f671df4737869a3820620b2c4e4428b41b0419
alcarazaina <alcarazdainara@gmail.com>
2024-04-30 11:55:17 +0200

    update

--- src/test/java/ejercicioscajanegra/ejercicio03/Ejercicio03Test.java
+++ src/test/java/ejercicioscajanegra/ejercicio03/Ejercicio03Test.java
@@ -11,12 +11,4 @@ class Ejercicio03Test {
     @DisplayName("Suma de números válida 55")
     void sumarValida() { assertEquals(55, Ejercicio03.sumar());
     }
-    @Test
-    @DisplayName("Suma de números inválida 54")
-    void sumarInvalida1() { assertNotEquals(54, Ejercicio03.sumar());
-    }
-    @Test
-    @DisplayName("Suma de números inválida 56")
-    void sumarInvalida2() { assertNotEquals(56, Ejercicio03.sumar());
-    }
 }
